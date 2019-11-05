ffe93b6d8bd69d249551d6ff4f14905bcab6cb49
dvminder <46944023+dvminder@users.noreply.github.com>
2019-11-05 13:17:31 -0600

    creation of the menu

---
+++ Assets/Script/menuChange.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class menuChange : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public void changeScene(string sceneName)
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+}
---
+++ Assets/name.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace UnityEngine.UI
+{
+    public class name : MonoBehaviour
+    {
+        public Button button;
+        public Text text;
+        // Start is called before the first frame update
+        void Awake()
+        {
+            button = GetComponent<Button>();
+            foreach (Transform t in transform)
+            { // might not be obvious
+                text = t.GetComponent<Text>();
+            }
+        }
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+    }
+}
