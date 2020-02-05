076ba00c2d1629089285c4bf7e00139372daedd1
gssimao <gsallessimao@hotmail.com>
2020-02-05 11:58:54 -0600

    version 1.42
    drag and drop working still need to connect with the other scripts

---
+++ Assets/Script/DragHandeler.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class DragHandeler : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler
+{
+    public static GameObject DraggedItem;
+    Vector3 startPosition;
+    Transform StartParent;
+    
+
+    public void OnBeginDrag(PointerEventData eventData)
+    {
+        DraggedItem = gameObject;
+        startPosition = transform.position;
+        StartParent = transform.parent;
+        GetComponent<CanvasGroup>().blocksRaycasts = false;
+    }
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        transform.position = Input.mousePosition;
+    }
+
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        DraggedItem = null;
+        GetComponent<CanvasGroup>().blocksRaycasts = true;
+        if (transform.parent == StartParent)
+        {
+            transform.position = startPosition;
+        }
+        
+    }
+}
---
+++ Assets/Script/SlotManager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class SlotManager : MonoBehaviour, IDropHandler
+{
+  public GameObject colors
+    {
+        get
+        {
+            if(transform.childCount>0)
+            {
+                return transform.GetChild(0).gameObject;
+            }
+            return null;
+        }
+
+    }
+
+    public void OnDrop(PointerEventData eventData)
+    {
+        if(!colors)
+        {
+            DragHandeler.DraggedItem.transform.SetParent(transform);
+        }
+    }
+}
