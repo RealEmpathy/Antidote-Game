355b7ae3826667363a88c5376a86175146e57058
dvminder <46944023+dvminder@users.noreply.github.com>
2020-04-01 08:39:25 -0500

    results menu added

---
+++ Assets/Script/results.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class results : MonoBehaviour
+{
+    public int totalNeutral = 0;
+    public int totalBad = 0;
+    public int totalGood = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void result()
+    {
+        totalNeutral = GetComponent<Flock>().currentNeutral;
+    }
+}
