05e82c2a0fb3dcb5c9e4db504af0ba46c6ddccce
gabe <gabriel.simao@trojans.dsu.edu>
2020-04-06 11:29:23 -0500

    version 1.54
    Implementing the new helix and color change. Still buggy

--- Assets/Script/SlotManager.cs
+++ Assets/Script/SlotManager.cs
@@ -2,10 +2,15 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
+using UnityEngine.UI;
 
 public class SlotManager : MonoBehaviour, IDropHandler
 {
-    public GameObject parent;
+    public GameObject ThisSlot;
+    public string number;
+    public bool update;
+
+
 
     public GameObject colors
     {
@@ -19,21 +24,79 @@ public class SlotManager : MonoBehaviour, IDropHandler
         }
 
     }
+    private void Start()
+    {
+        if(this.gameObject.tag == "Nodrop")
+        {
+
+        }
+        else
+        {
+            ThisSlot.gameObject.GetComponent<Image>().color = new Color(1, 1, 1, 1);
+        }
+        
+    }
 
     public void OnDrop(PointerEventData eventData)
     {
         if(!colors)
         {
-            DragHandeler.DraggedItem.transform.SetParent(transform);
+            update = false;
+            DragHandeler.DraggedItem.transform.SetParent(transform); // this makes the object stay in the slots on the right
+          
+            DragHandeler.DraggedItem.GetComponent<Image>().color = new Color(0, 0, 0, 0);
+            number = ThisSlot.gameObject.transform.GetChild(0).gameObject.name;
+            UpdateColor(number);
         }
         else
         {
-            if(this.gameObject.tag != "Nodrop")
+            update = true;
+            if (this.gameObject.tag != "Nodrop")
             {
                 Destroy(transform.GetChild(0).gameObject);
                 DragHandeler.DraggedItem.transform.SetParent(transform);
+                ThisSlot.gameObject.GetComponent<Image>().color = new Color(1, 1, 1, 1);
             }
             //parent.transform.GetChild(0).gameObject;
+
+            DragHandeler.DraggedItem.GetComponent<Image>().color = new Color(0, 0, 0, 0); // make the dragged object invisible
+            number = ThisSlot.gameObject.transform.GetChild(0).gameObject.name;
+            UpdateColor(number);
+            Debug.Log(ThisSlot.gameObject.GetComponent<Image>().color);
         }
     }
+
+    private void UpdateColor (string number)
+    {
+        if (number == "1")
+        {
+            ThisSlot.gameObject.GetComponent<Image>().color = new Color(0, 0, 1, 1);
+        }
+        else
+           if (number == "2")
+        {
+            ThisSlot.gameObject.GetComponent<Image>().color = new Color(0, 1, 0, 1);
+        }
+        else
+           if (number == "3")
+        {
+            ThisSlot.gameObject.GetComponent<Image>().color = new Color(1, 0, 0, 1);
+        }
+        else
+           if (number == "4")
+        {
+            ThisSlot.gameObject.GetComponent<Image>().color = new Color(0, 0, 0, 1);
+        }
+        else
+           if (number == "5")
+        {
+            ThisSlot.gameObject.GetComponent<Image>().color = new Color(1, 1, 0, 1);
+        }
+        
+    }
+/*    private void Update()
+    {
+        if(update)
+            UpdateColor(number);
+    }*/
 }
